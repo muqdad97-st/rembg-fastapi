3468adc8910d7f5241f97ef574f342d874e387d3
muqdad97 <muqdad9741@gmail.com>
2025-08-03 21:18:25 +0200

    Enable Swagger UI docs

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ from fastapi.responses import StreamingResponse
 from rembg import remove
 from io import BytesIO
 
-app = FastAPI()
-
-@app.get("/")
-def home():
-    return {"message": "🎉 Rembg FastAPI server is live!"}
+# 👇 هذا السطر مهم لتفعيل /docs
+app = FastAPI(docs_url="/docs", redoc_url="/redoc", openapi_url="/openapi.json")
 
 @app.post("/remove")
 async def remove_bg(file: UploadFile = File(...)):
     input_image = await file.read()
     output_image = remove(input_image)
     return StreamingResponse(BytesIO(output_image), media_type="image/png")
+
+@app.get("/")
+def root():
+    return {"message": "🎉 Rembg FastAPI server is live!"}
